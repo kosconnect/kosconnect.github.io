86d6a9360e4015f21bfa9654377cd7be1495294c
fathyafathazz <fathyafathazzra@gmail.com>
2025-01-27 17:53:22 +0700

    feat: update transaction detail rendering in invoice.js for improved error handling and user feedback

--- js/invoice.js
+++ js/invoice.js
@@ -1,124 +1,113 @@
 // Import getCookie dan renderHeader dari header.js
 import { getCookie, renderHeader } from "./header.js";
 
-// Menangkap parameter transaction_id dari URL
-
-// Gunakan transactionId untuk mengambil dan menampilkan detail transaksi
-// fetch(`https://kosconnect-server.vercel.app/api/transaction/${transactionId}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     // Render detail transaksi di halaman invoice
-//     console.log(data); // Anda dapat menggantinya dengan logika untuk menampilkan data di halaman
-//   })
-//   .catch(error => console.error("Gagal mengambil detail transaksi:", error));
-
-// Variabel global untuk menyimpan semua data orders
-
-// Fungsi untuk merender kartu transaksi ke halaman
-async function renderTransactionDetail(orders) {
+// Fungsi untuk merender detail transaksi ke halaman
+async function renderTransactionDetail(order) {
   const orderHistoryElement = document.getElementById("orderHistory");
 
   // Bersihkan elemen sebelumnya
   orderHistoryElement.innerHTML = "";
 
-  if (orders.length === 0) {
+  if (!order) {
     orderHistoryElement.innerHTML =
-      "<p>Tidak ada transaksi yang ditemukan.</p>";
+      "<p>Transaksi tidak ditemukan atau data kosong.</p>";
     return;
   }
-  for (const order of orders.data) {
-    // Ambil detail kos langsung dari endpoint
-    const response = await fetch(
-      `https://kosconnect-server.vercel.app/api/rooms/${order.room_id}/detail`
-    );
 
-    if (!response.ok) {
-      console.error(`Error fetching room detail for room_id ${order.room_id}`);
-      continue; // Lewati jika detail kos tidak ditemukan
+  // Ambil detail kos langsung dari endpoint
+  const response = await fetch(
+    `https://kosconnect-server.vercel.app/api/rooms/${order.room_id}/detail`
+  );
+
+  if (!response.ok) {
+    console.error(`Error fetching room detail for room_id ${order.room_id}`);
+    orderHistoryElement.innerHTML =
+      "<p>Gagal mengambil detail kos untuk transaksi ini.</p>";
+    return;
+  }
+
+  const roomDetail = await response.json();
+
+  // Data boarding house, room, category, owner langsung diambil dari roomDetail
+  const { boarding_house, owner, room_type } = roomDetail[0];
+  const category = boarding_house?.category || null;
+
+  // Format tanggal
+  const formattedCheckInDate = new Date(
+    order.check_in_date
+  ).toLocaleDateString("id-ID", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedUpdatedAt = new Date(order.updated_at).toLocaleDateString(
+    "id-ID",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
     }
-
-    const roomDetail = await response.json();
-
-    // Data boarding house, room, category, owner langsung diambil dari roomDetail
-    const { boarding_house, owner, room_type } = roomDetail[0];
-    const category = boarding_house?.category || null;
-
-    // Format tanggal
-    const formattedCheckInDate = new Date(
-      order.check_in_date
-    ).toLocaleDateString("id-ID", {
+  );
+  const formattedCreatedAt = new Date(order.created_at).toLocaleDateString(
+    "id-ID",
+    {
       day: "2-digit",
       month: "long",
       year: "numeric",
-    });
+      hour: "2-digit",
+      minute: "2-digit",
+    }
+  );
 
-    const formattedUpdatedAt = new Date(order.updated_at).toLocaleDateString(
-      "id-ID",
-      {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-      }
-    );
-    const formattedCreatedAt = new Date(order.created_at).toLocaleDateString(
-      "id-ID",
-      {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-      }
-    );
+  const customFacilities = order.custom_facilities
+    .map(
+      (facility) =>
+        `<p>${facility.name} <span>- Rp${facility.price.toLocaleString(
+          "id-ID"
+        )}</span></p>`
+    )
+    .join("");
 
-    const customFacilities = order.custom_facilities
-      .map(
-        (facility) =>
-          `<p>${facility.name} <span>- Rp${facility.price.toLocaleString(
-            "id-ID"
-          )}</span></p>`
-      )
-      .join("");
+  const paymentStatusMap = {
+    pending: "Menunggu",
+    settlement: "Lunas",
+    expire: "Kadaluarsa",
+    deny: "Ditolak atau Gagal",
+  };
 
-    const paymentStatusMap = {
-      pending: "Menunggu",
-      settlement: "Lunas",
-      expire: "Kadaluarsa",
-      deny: "Ditolak atau Gagal",
-    };
+  const paymentStatus =
+    paymentStatusMap[order.payment_status] || "Tidak Diketahui";
 
-    const paymentStatus =
-      paymentStatusMap[order.payment_status] || "Tidak Diketahui";
+  // Mapping untuk payment term ke bahasa Indonesia
+  const paymentTermMapping = {
+    monthly: "Bulanan",
+    quarterly: "Per 3 Bulan",
+    semi_annual: "Per 6 Bulan",
+    yearly: "Tahunan",
+  };
 
-    // Mapping untuk payment term ke bahasa Indonesia
-    const paymentTermMapping = {
-      monthly: "Bulanan",
-      quarterly: "Per 3 Bulan",
-      semi_annual: "Per 6 Bulan",
-      yearly: "Tahunan",
-    };
+  const paymentTermText =
+    paymentTermMapping[order.payment_term] || "Tidak Diketahui";
 
-    const paymentTermText =
-      paymentTermMapping[order.payment_term] || "Tidak Diketahui";
-
-    // Komponen informasi kos
-    const kosDetails = `
+  // Komponen informasi kos
+  const kosDetails = `
 <p><strong> ${roomDetail[0]?.room_name || "Tidak Diketahui"}</strong></p>
 <p><strong>Kategori:</strong> ${
-      roomDetail[0]?.category_name || "Tidak Diketahui"
-    }</p>
+    roomDetail[0]?.category_name || "Tidak Diketahui"
+  }</p>
 <p><strong>Alamat:</strong> ${
-      roomDetail[0]?.boarding_house?.address || "Tidak Diketahui"
-    }</p>
+    roomDetail[0]?.boarding_house?.address || "Tidak Diketahui"
+  }</p>
 <p><strong>Nama Pemilik:</strong> ${
-      roomDetail[0]?.owner?.fullname || "Tidak Diketahui"
-    }</p>
+    roomDetail[0]?.owner?.fullname || "Tidak Diketahui"
+  }</p>
 `;
 
-    // Komponen rincian biaya
-    const biayaDetails = ` 
+  // Komponen rincian biaya
+  const biayaDetails = ` 
       <p><strong>Harga Sewa:</strong> Rp ${order.price.toLocaleString(
         "id-ID"
       )} / ${paymentTermText}</p>
@@ -131,24 +120,24 @@ async function renderTransactionDetail(orders) {
       )}</p>
     `;
 
-    // Tombol bayar sekarang (jika status pending)
-    let actionElement = "";
-    if (order.payment_status === "pending") {
-      actionElement = ` 
+  // Tombol bayar sekarang (jika status pending)
+  let actionElement = "";
+  if (order.payment_status === "pending") {
+    actionElement = ` 
         <a href="https://kosconnect-server.vercel.app/transactions/${order.transaction_id}/payment" class="pay-now-button">
           Bayar Sekarang
         </a>`;
-    } else if (order.payment_status === "settlement") {
-      actionElement = ` 
+  } else if (order.payment_status === "settlement") {
+    actionElement = ` 
         <p><strong>Metode Pembayaran:</strong> ${order.payment_method}</p>
         <p class="updated-at">Diupdate: ${formattedUpdatedAt}</p>`;
-    }
+  }
 
-    // Buat elemen order-card
-    const card = document.createElement("div");
-    card.className = "order-card";
+  // Buat elemen order-card
+  const card = document.createElement("div");
+  card.className = "order-card";
 
-    card.innerHTML = `
+  card.innerHTML = `
       <div class="left-section">
         <div class="left-header">
           <div class="brand-logo">
@@ -169,14 +158,8 @@ async function renderTransactionDetail(orders) {
         ${actionElement}
       </div>
     `;
-    // Menambahkan event listener untuk klik kartu
-    card.addEventListener("click", () => {
-      // Arahkan ke halaman detail transaksi dengan ID transaksi
-      window.location.href = `https://kosconnect-server.vercel.app/transactions/${order.transaction_id}`;
-    });
 
-    orderHistoryElement.appendChild(card);
-  }
+  orderHistoryElement.appendChild(card);
 
   // Atur ikon status setelah rendering
   setStatusIcons();
@@ -215,6 +198,7 @@ function setStatusIcons() {
     }
   });
 }
+
 // Ambil data kos saat halaman dimuat
 window.onload = async () => {
   try {
@@ -230,8 +214,12 @@ window.onload = async () => {
         },
       }
     );
-    const transactionData = await response.json();
 
+    if (!response.ok) {
+      throw new Error("Gagal mengambil data transaksi");
+    }
+
+    const transactionData = await response.json();
     await renderTransactionDetail(transactionData);
 
     const userRole = getCookie("userRole");
