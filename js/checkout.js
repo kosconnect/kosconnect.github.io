97bced9c5e7a4ceed19ea06d61f26d8ea0feb167
fathyafathazz <fathyafathazzra@gmail.com>
2025-02-02 03:31:57 +0700

    refactor: remove payment redirection logic from checkout.js for cleaner flow

--- js/checkout.js
+++ js/checkout.js
@@ -429,17 +429,3 @@ window.onload = () => {
     console.error("room_id not found in URL.");
   }
 };
-
-// Tangkap URL setelah pembayaran
-const urlParams = new URLSearchParams(window.location.search);
-const transactionStatus = urlParams.get("transaction_status");
-const transactionId = urlParams.get("order_id"); // Atau sesuaikan dengan response Midtrans
-
-// Cek status pembayaran
-if (transactionStatus === "settlement") {
-  // Jika pembayaran berhasil, arahkan ke halaman invoice
-  window.location.href = `https://kosconnect.github.io/invoice.html?transaction_id=${transactionId}`;
-} else {
-  // Jika pembayaran gagal atau pending, arahkan ke history
-  window.location.href = "https://kosconnect.github.io/history.html";
-}
